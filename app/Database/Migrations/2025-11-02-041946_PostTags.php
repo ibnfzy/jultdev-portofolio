1de15326be8e72c82d1aa72acd662c5b8d2941b4
Ibnu Fauzy <58937373+ibnfzy@users.noreply.github.com>
2025-11-02 12:20:04 +0800

    tw

---
+++ app/Database/Migrations/2025-11-02-041946_PostTags.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class PostTags extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
